60bbbc451d63b205e31561029a6c2130cc331315
shaik9133 <sk.md.bashid9133@gmail.com>
2024-07-17 22:56:10 +0530

    added

--- bookstore/src/App.jsx
+++ bookstore/src/App.jsx
@@ -10,19 +10,17 @@ function App() {
   const [authUser] = useAuth();
   console.log(authUser);
   return (
-    <>
-      <div className="dark:bg-slate-900 dark:text-white">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/course"
-            element={authUser ? <Courses /> : <Navigate to="/signup" />}
-          />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-        <Toaster />
-      </div>
-    </>
+    <div className="dark:bg-slate-900 dark:text-white">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/course"
+          element={authUser ? <Courses /> : <Navigate to="/signup" />}
+        />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+      <Toaster />
+    </div>
   );
 }
 
--- bookstore/src/context/AuthProvider.jsx
+++ bookstore/src/context/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useContext, useState } from "react";
 
 export const AuthContext = createContext();
+
 export default function AuthProvider({ children }) {
   const initialAuthUser = localStorage.getItem("Users");
   const [authUser, setAuthUser] = useState(
@@ -12,4 +13,5 @@ export default function AuthProvider({ children }) {
     </AuthContext.Provider>
   );
 }
+
 export const useAuth = () => useContext(AuthContext);
